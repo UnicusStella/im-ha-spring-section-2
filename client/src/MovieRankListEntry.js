f2e5894ef116c9cd6d423c3752316fdc5b16cf92
UnicusStella <unicusstella@gmail.com>
2021-08-18 17:46:03 +0900

    HA2 Final-Client Submit

--- client/src/MovieRankListEntry.js
+++ client/src/MovieRankListEntry.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function MovieRankListEntry({
   handleCardClick,
   movie,
-  movie: { runtime, rating, title, medium_corver_image, genres },
+  movie: { runtime, rating, title, medium_cover_image, genres },
 }) {
   return (
     <div
@@ -13,7 +13,7 @@ export default function MovieRankListEntry({
       }}
     >
       <div style={{ flex: 3 }}>
-        <img width="100%" height="100%" src={medium_corver_image} alt="cover" />
+        <img width="100%" height="100%" src={medium_cover_image} alt="cover" />
       </div>
       <div style={{ flex: 7 }}>
         <h3 className="title">{title}</h3>
